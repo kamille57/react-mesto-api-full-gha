4eebd1d2202bfbde074563596dab80fb69afa42f
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-11-03 00:41:55 +0300

    рлофилм

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,12 @@ const app = express();
 
 app.use(helmet());
 
+app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://kamille57.nomoredomainsrocks.ru', 'https://api.kamille57.nomoredomainsrocks.ru'], credentials: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://kamille57.nomoredomainsrocks.ru', 'https://api.kamille57.nomoredomainsrocks.ru'], credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
