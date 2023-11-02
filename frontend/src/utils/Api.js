e8a58277ba2db5b5547ba7002e957b4812a9215c
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-11-02 22:14:56 +0300

    sdfs

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -35,13 +35,13 @@ class Api {
     });
   }
 
-  updateAvatar(avatar) {
+  updateAvatar(data) {
     return this._request(`${this.baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       credentials: 'include',
       headers: this.headers,
       body: JSON.stringify({
-        avatar: avatar
+        avatar: data.avatar
       })
     });
   }
